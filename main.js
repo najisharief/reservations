4d34b5fbd818140763a2e8628997729fb42aa434
Naji <naji.sharief@gmail.com>
2018-04-11 17:15:24 +0300

    reservation 2

--- main.js
+++ main.js
@@ -7,6 +7,16 @@ var name = prompt('Please enter the name for your reservation');
 
 var claimReservation = function () {
   // write your code here!
+if (name in reservations && reservations[name].claimed){
+  alert("You have used your Reservation before");
+return;
 }
-
+else if (name in reservations && reservations[name].claimed == false){
+    alert("Welcome "+ name);
+    return;
+}
+else {
+  alert("You have no Reservation");
+}
+}
 claimReservation();
